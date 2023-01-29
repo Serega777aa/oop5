e63dffebd479488f284e5b0fc264f68053ef1746
Your Name <serega777aa@mail.ru>
2023-01-29 18:31:06 +0300

    seminar5

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+//        Main run = new Main();
+//        run.ExportFile(ArrayList<Phonebook> pb);
+//        Filemanager run = new Filemanager();
+//        Addressbook phones = new Addressbook();
+//        phones.Addressbook();
+////        run.ExportFile(phones.pb);
+////        System.out.println(phones.pb);
+//        run.ImportFile(phones.pb);
+//        System.out.println("****************");
+//        phones.showAddressBook();
+
+        Menu.mainMenu();
+
+    }
+
+
+}
+
